63f067484de2e7d06694fa46f50b23bb4e3c7c7c
wildeys <wildhaan11@gmail.com>
2026-08-18 01:04:26 +0500

    Remove unused components including buttons, card, colors, inputs, title, typography, and various UI elements to streamline the codebase and improve maintainability.

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -2,18 +2,13 @@ import { cssInterop } from 'nativewind';
 import Svg from 'react-native-svg';
 
 export * from './button';
-export * from './checkbox';
 export { default as colors } from './colors';
 export * from './focus-aware-status-bar';
-export * from './image';
 export * from './input';
-export * from './list';
 export * from './modal';
-export * from './progress-bar';
 export * from './select';
 export * from './text';
 export * from './themed-rn';
-export * from './utils';
 
 export { ActivityIndicator, TouchableOpacity } from 'react-native';
 export { SafeAreaView } from 'react-native-safe-area-context';
